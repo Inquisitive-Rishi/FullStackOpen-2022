08303aad4efa1d3283f55a43b86994fef30f9184
Rishi Raj <rishiraj007bgp@gmail.com>
2023-11-29 12:06:57 +0530

    finish1.6

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+const LargeTxt = ({txt}) => {
+  return (
+    <>
+      <h1>{txt}</h1>
+    </>
+  )
+}
+
+const Button = ({onClick,txt}) => {
+  return ( 
+    <>
+      <button onClick={onClick}>{txt}</button>
+    </>
+  )
+}
+
+const CountStat = ({ txt, value }) => {
+  return (
+    <>
+      <p>{txt}:{value}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const [good,setGood] = useState(0)
+  const [neutral,setNeutral] = useState(0)
+  const [bad,setBad] = useState(0)
+
+  return (
+    <>
+      <LargeTxt txt='Give feedback'/>
+      <Button onClick={() => setGood(good+1)} txt='good'/>
+      <Button onClick={() => setNeutral(neutral+1)} txt='neutral'/>
+      <Button onClick={() => setBad(bad+1)} txt='bad'/>
+      <LargeTxt txt='statistics'/>
+      <CountStat txt='good' value={good}/>
+      <CountStat txt='neutral' value={neutral}/>
+      <CountStat txt='bad' value={bad}/>
+    </>
+  )
+}
+
+export default App;
