6eaad8c6304c6dafaf7fe5e3ba7acd516c88a525
Rishi Raj <rishiraj007bgp@gmail.com>
2023-12-10 11:31:26 +0530

    get country name on button click

--- part2/data_for_countries/src/App.jsx
+++ part2/data_for_countries/src/App.jsx
@@ -1,6 +1,27 @@
+import axios from "axios"
+import { useEffect } from "react"
+
 const App = () => {
+
+  const countries = []
+  const getCountriesName = () => {
+    axios.get('https://studies.cs.helsinki.fi/restcountries/api/all')
+    .then(res => {
+      const names = res.data;
+      names.map(name => countries.push(name.name.common))
+    })
+  }
+  
+  
+
   return (
-    <h1>Hello</h1>
+    <>
+    <button onClick={getCountries}>get countries</button>
+    <label htmlFor="country">
+      find countries: 
+      <input type="text" id="country"/>
+    </label>
+    </>
   )
 }
 
