b3c0c680afb55a9b90fb28598d1635f9c41299f5
Rishi Raj <rishiraj007bgp@gmail.com>
2023-12-14 17:37:30 +0530

    update port variable to dynamic env

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -90,6 +90,6 @@ app.delete('/api/persons/:id', (req, res) => {
   console.log(persons);
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT)
 console.log(`server is listening to port ${PORT}`);
