3a02b4730a46242359271070a2985677521181dc
Rishi Raj <rishiraj007bgp@gmail.com>
2023-12-21 13:05:42 +0530

    finish structure of backend

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -6,14 +6,6 @@ const app = require('./app');
 
 app.use(express.static('dist'));
 
-app.get('/', (req, res) => {
-  res.send('Hello welcome to this server');
-});
-
-app.get('/info', (req, res) => {
-  const currDT = new Date();
-  res.send(`phonebook <br/> ${currDT}`);
-});
 
 const { PORT } = process.env;
 app.listen(PORT);
